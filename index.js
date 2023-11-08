ac52b1bdaf4e3601b4bfb0fdbfaf21b1b0224661
Sreeni15 <55984290+Sreeni15@users.noreply.github.com>
2023-11-08 17:27:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 9000;
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World from Docker and Node.js with Jenkins Pipeline!</h1>');
+    res.send('<h1>Hello World from Docker and Node.js with Jenkins Declarative Pipeline!
+             </h1>');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
